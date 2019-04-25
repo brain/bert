38c9b7bf6ae458001838c7ffddc5680d4ed5c665
Mathew Teoh <mteoh@brain.im>
2019-04-25 16:13:18 -0700

    initial setup of the inference api

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from app.extensions import api
 from app.controllers import (
-    health,
+    health, siamese_bert_similarity
 )
 
 
 # Health Routes
 api.add_resource(health.HealthResource, '/', '/health')
+
+
+# Inference
+# TODO: when done, replace SiameseBertSimilarityPlaceholder with SiameseBertSimilarity
+api.add_resource(
+    siamese_bert_similarity.SiameseBertSimilarityPlaceholder,
+    '/', '/similarities')
+
+# TODO: remove when done
+api.add_resource(
+    siamese_bert_similarity.SiameseBertSimilarity,
+    '/', '/similarities_dev')
