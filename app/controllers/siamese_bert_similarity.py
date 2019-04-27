618a21df866ac389acb5c5213c1e09c734915ab4
Mathew Teoh <mteoh@brain.im>
2019-04-26 17:43:08 -0700

    correct model being loaded this time

--- app/controllers/siamese_bert_similarity.py
+++ app/controllers/siamese_bert_similarity.py
@@ -2,9 +2,29 @@
 from flask_restful import (
     Resource, reqparse
 )
-from training.siamese_bert import SiameseBert
 from operator import itemgetter
 import time
+import os
+import tensorflow as tf
+import pandas as pd
+from training.siamese_modeling import create_model
+from training.ftm_processor import FtmProcessor
+from training.input_fns import input_fn_builder
+from training import featurization
+from google_bert import modeling
+from google_bert import tokenization
+
+
+BERT_MODEL = 'uncased_L-12_H-768_A-12'
+BERT_PRETRAINED_DIR = 'gs://cloud-tpu-checkpoints/bert/' + BERT_MODEL
+TRAINED_MODEL_BUCKET = 'bert_output_bucket_mteoh'
+TASK = 'FTM_BERT_DATA_009_tpu_trial_1'
+TRAINED_MODEL_DIR = 'gs://{}/bert/models/{}'.format(TRAINED_MODEL_BUCKET, TASK)
+PREDICT_BATCH_SIZE = 256
+RANDOM_PROJECTION_OUTPUT_DIM = 128
+MAX_SEQ_LENGTH = 30
+USE_TPU = False
+SAVE_CHECKPOINT_STEPS = 1000
 
 
 class SiameseBertSimilarityPlaceholder(Resource):
@@ -13,28 +33,40 @@ class SiameseBertSimilarityPlaceholder(Resource):
 
 
 class SiameseBertSimilarity(Resource):
-    BERT_MODEL = 'uncased_L-12_H-768_A-12'
-    BERT_PRETRAINED_DIR = 'gs://cloud-tpu-checkpoints/bert/' + BERT_MODEL
-    OUTPUT_BUCKET = 'bert_output_bucket_mteoh'
-    BERT_TFRECORD_BUCKET = 'gs://mteoh_siamese_bert_data'
-    TASK = 'FTM_BERT_DATA_009_tpu_trial_1'
-    OUTPUT_DIR = 'gs://{}/bert/models/{}'.format(OUTPUT_BUCKET, TASK)
-    PREDICT_BATCH_SIZE = 256
-    MAX_SEQ_LENGTH = 30
-    USE_TPU = False
 
-    sb = SiameseBert(
-        bert_model_type=BERT_MODEL,
-        bert_pretrained_dir=BERT_PRETRAINED_DIR,
-        output_dir=OUTPUT_DIR,
-        predict_batch_size=PREDICT_BATCH_SIZE,
-        max_seq_length=MAX_SEQ_LENGTH,
-        use_tpu=USE_TPU)
+    def __init__(self):
+        super()
 
-    post_parser = reqparse.RequestParser()
-    post_parser.add_argument("doc1", action='append', required=True)
-    post_parser.add_argument("doc2", action='append', required=True)
-    post_parser.add_argument("sort", required=False)
+        # general BERT model related things
+        vocab_file = os.path.join(BERT_PRETRAINED_DIR, 'vocab.txt')
+        config_file = os.path.join(BERT_PRETRAINED_DIR, 'bert_config.json')
+        do_lower_case = BERT_MODEL.startswith('uncased')
+
+        # checkpoint to initialize from
+        checkpoint_path = tf.train.latest_checkpoint(TRAINED_MODEL_DIR)
+        # checkpoint_path = os.path.join(BERT_PRETRAINED_DIR, 'bert_model.ckpt')
+
+        # featurization related tools
+        self.processor = FtmProcessor()
+        self.label_list = self.processor.get_labels()
+        self.tokenizer = tokenization.FullTokenizer(
+            vocab_file=vocab_file,
+            do_lower_case=do_lower_case)
+
+        # estimator setup
+        run_config = tf.estimator.RunConfig()
+        model_fn = self._model_fn_builder(
+            bert_config=modeling.BertConfig.from_json_file(config_file),
+            init_checkpoint=checkpoint_path)
+        self.estimator = tf.estimator.Estimator(
+            model_fn=model_fn,
+            config=run_config)
+
+        # set up parser
+        self.post_parser = reqparse.RequestParser()
+        self.post_parser.add_argument("doc1", action='append', required=True)
+        self.post_parser.add_argument("doc2", action='append', required=True)
+        self.post_parser.add_argument("sort", required=False)
 
     def post(self):
         '''Calculates similarities between all combination of parameters
@@ -68,9 +100,8 @@ class SiameseBertSimilarity(Resource):
         print(f'doc: {doc}; docs: {docs}')
         start = time.time()
         num_comparisons = len(docs)
-        df_result = self.sb.predict_pairs(
-            [doc] * num_comparisons,
-            docs, drop_remainder=False)
+        df_result = self._predict_pairs(
+            [doc] * num_comparisons, docs)
         sim_scores = df_result.sim_scores.tolist()
         indices = df_result.index.values.tolist()
         results = [
@@ -81,3 +112,54 @@ class SiameseBertSimilarity(Resource):
         if sort:
             results = sorted(results, key=itemgetter('score'), reverse=True)
         return results
+
+    def _predict_pairs(self, l_queries, r_queries):
+        pred_features = featurization.convert_examples_to_features(
+            self.processor._create_examples(l_queries, r_queries),
+            MAX_SEQ_LENGTH, self.tokenizer)
+        input_fn = input_fn_builder(
+            features=pred_features,
+            seq_length=MAX_SEQ_LENGTH,
+            is_training=False,
+            drop_remainder=False,
+            provided_batch_size=PREDICT_BATCH_SIZE)
+        pred_results = list(self.estimator.predict(input_fn=input_fn))
+        pred_results = pd.DataFrame.from_records(pred_results)
+
+        return pred_results
+
+    # TODO: is there a way we can have just one model fn for both training and
+    # this API?
+    def _model_fn_builder(self, bert_config, init_checkpoint):
+
+        def model_fn(features, labels, mode, params):
+
+            l_input_ids = features["l_input_ids"]
+            r_input_ids = features["r_input_ids"]
+            l_input_mask = features["l_input_mask"]
+            r_input_mask = features["r_input_mask"]
+
+            (total_loss, per_example_loss, logits, sim_scores, label_preds,
+                merged_summaries) = create_model(
+                bert_config, False, l_input_ids, r_input_ids,
+                l_input_mask, r_input_mask, labels, True,
+                RANDOM_PROJECTION_OUTPUT_DIM, False, False)
+
+            # initialize variables
+            print(f'---- initializing from: {init_checkpoint}')
+            tvars = tf.trainable_variables()
+            initialized_variable_names = {}
+            assignment_map, initialized_variable_names = \
+                modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+            with tf.name_scope('assignments'):
+                tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+
+            # output specfication
+            output_spec = tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions={"sim_scores": sim_scores,
+                             "label_preds": label_preds})
+
+            return output_spec
+
+        return model_fn
